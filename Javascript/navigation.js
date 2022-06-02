15c81f3a8386e7ed88df07a5f66dd15f2c43b731
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-06-02 20:45:15 +0200

    added repo name back to js

--- Javascript/navigation.js
+++ Javascript/navigation.js
@@ -65,19 +65,19 @@ function navigate5(pages){
     switch(pages){
         case 1:
             console.log("tostring here");
-            window.location.href = "../index.html";
+            window.location.href = "WSOA3028_Assignment2PortfolioWebsite/index.html";
             break;
         case 2:
-            window.location.href = "../Games/games.html";
+            window.location.href = "WSOA3028_Assignment2PortfolioWebsite/Games/games.html";
             break;
         case 3:
-            window.location.href = "../Code Projects/codeProjects.html";
+            window.location.href = "WSOA3028_Assignment2PortfolioWebsite/Code Projects/codeProjects.html";
             break;
         case 4:
-            window.location.href = "../Blog/blog.html";
+            window.location.href = "WSOA3028_Assignment2PortfolioWebsite/Blog/blog.html";
             break;
         case 5:
-            window.location.href = "../Design Doc Page/design.html";
+            window.location.href = "WSOA3028_Assignment2PortfolioWebsite/Design Doc Page/design.html";
             break; 
     }
 }
